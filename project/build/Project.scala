374f11e336b55d4f1e68fbd7fd6fb5b1a2231cb7
thoraage <thoraageeldby@gmail.com>
2011-02-02 22:03:46 +0100

    * Added postgresql driver and setup for it in staging

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -16,6 +16,7 @@ class Project(info: ProjectInfo) extends DefaultWebProject(info) {
   val httpDispatch = "net.databinder" %% "dispatch-http" % "0.7.8" withSources
   val jodaTime     = "joda-time" % "joda-time" % "1.6.2" withSources
   val h2           = "com.h2database" % "h2" % "1.2.138" withSources
+  val postgresql   = "postgresql" % "postgresql" % "8.4-702.jdbc3" withSources
 
   val jettyTest    = "org.mortbay.jetty" % "jetty" % "6.1.22" % "test" withSources
   val specs        = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test" withSources
