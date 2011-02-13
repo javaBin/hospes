6b7b3ac0429181ce96cb814ee7dc6525f9e66a5e
thoraage <thoraageeldby@gmail.com>
2011-02-13 18:31:27 +0100

    * Cleaned up some mess with english/norwegian texts * Changed text of paid / not paid membership * Moved membership information under name

--- src/main/scala/javaBin/snippet/UserInformation.scala
+++ src/main/scala/javaBin/snippet/UserInformation.scala
@@ -9,8 +9,8 @@ class UserInformation {
   def current(xhtml: NodeSeq): NodeSeq =
     Person.currentUser.map {
       user =>
-        Text(if (user.hasActiveMembership) S.?("membership.has") else S.?("membership.has.not")) ++
-        Text(": " + user.niceName) ++
+        Text(user.niceName) ++
+        <br/> ++ Text(if (user.hasActiveMembership) S.?("membership.has") else S.?("membership.has.not")) ++
         <br/><a href={Person.logoutPath.mkString("/", "/", "")}><lift:loc.logout/></a>
     }.openOr{
       Text(S.?("user.not.logged.in")) ++
